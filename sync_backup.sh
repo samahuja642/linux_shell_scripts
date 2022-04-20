beb96c08354e2bdc425c9941dc765a8e738475a9
Samarth_Ahuja <samahuja642@gmail.com>
2022-04-20 14:22:07 +0530

    Update sync_backup.sh

--- sync_backup.sh
+++ sync_backup.sh
@@ -5,3 +5,8 @@ then
     rm -r ~/Downloads/material/temp/*
 fi
 poweroff
+
+# /mnt/backup is the path for backup mounted drive you should change it accordingly
+# temp folder path should also be changed
+
+# This Script will Sync the drive into another drive and delete the one time required data on every shutdown.Not Much Helpful in Servers , but its only for casual or daily use.
